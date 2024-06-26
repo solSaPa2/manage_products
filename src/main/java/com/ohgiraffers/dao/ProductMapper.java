59bc1f258ad3d01dd3403b18d021c91aa85e325d
qwes5674 <qwes5674@gmail.com>
2024-06-26 15:03:14 +0900

    [FEAT] Delete Product 작성
    Add Product 작성

--- src/main/java/com/ohgiraffers/dao/ProductMapper.java
+++ src/main/java/com/ohgiraffers/dao/ProductMapper.java
@@ -1,4 +1,12 @@
 package com.ohgiraffers.dao;
 
+import com.ohgiraffers.dto.ProductDTO;
+
+import java.util.List;
+
 public interface ProductMapper {
+
+    List<ProductDTO> selectAllProduct();
+
+    List<ProductDTO> deleteProduct();
 }
--- src/main/java/com/ohgiraffers/service/ProductService.java
+++ src/main/java/com/ohgiraffers/service/ProductService.java
@@ -1,4 +1,47 @@
 package com.ohgiraffers.service;
 
+import com.ohgiraffers.dao.ProductMapper;
+import com.ohgiraffers.dto.ProductDTO;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
+
 public class ProductService {
+
+    private ProductMapper mapper;
+
+    public List<ProductDTO> selectAllProducts() {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(ProductMapper.class);
+
+        List<ProductDTO> productList = mapper.selectAllProduct();
+
+        for(ProductDTO productDTO : productList){
+            System.out.println(productDTO);
+        }
+
+        sqlSession.close();
+        return productList;
+    }
+
+    public List<ProductDTO> deleteProduct(){
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(ProductMapper.class);
+
+        List<ProductDTO> productList = mapper.deleteProduct();
+
+        for(ProductDTO productDTO : productList){
+            System.out.println(productDTO);
+        }
+
+        sqlSession.close();
+        return deleteProduct();
+    }
+
+
+
 }
