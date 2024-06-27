0b9445762f3a746c18c10da176720fccccf91de6
qwes5674 <qwes5674@gmail.com>
2024-06-27 10:56:17 +0900

    [FEATURE] infoAll Product 추가
    infoAll Product 추가

--- src/main/java/com/ohgiraffers/service/ProductService.java
+++ src/main/java/com/ohgiraffers/service/ProductService.java
@@ -4,6 +4,8 @@ import com.ohgiraffers.dao.ProductMapper;
 import com.ohgiraffers.dto.ProductDTO;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.List;
+
 import static com.ohgiraffers.common.Template.getSqlSession;
 
 public class ProductService {
@@ -25,5 +27,52 @@ public class ProductService {
 
         sqlSession.close();
         return result > 0 ? true : false;
+    }
+
+    public boolean deleteProduct(int code){
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(ProductMapper.class);
+
+        int result = mapper.deleteProduct(code);
+
+        if(result > 0){
+            sqlSession.commit();
+        } else {
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+        return result > 0 ? true : false;
+    }
+
+
+    public boolean updateProduct(ProductDTO productDTO) {
+
+        SqlSession sqlSession = getSqlSession();
+
+        mapper = sqlSession.getMapper(ProductMapper.class);
+
+        int result = mapper.updateProduct(productDTO);
+
+        if(result > 0){
+            sqlSession.commit();
+        } else {
+            sqlSession.rollback();
+        }
+
+        sqlSession.close();
+        return result > 0 ? true : false;
+    }
+
+    public List<ProductDTO> selectAllProducts() {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(ProductMapper.class);
+
+        List<ProductDTO> productList = mapper.selectAllProduct();
+
+        sqlSession.close();
+        return productList;
     }
 }
--- src/main/java/com/ohgiraffers/view/ProductView.java
+++ src/main/java/com/ohgiraffers/view/ProductView.java
@@ -21,10 +21,10 @@ public class ProductView {
 
             switch (no){
                 case 1: productController.insertProduct(inputMenu()); break;
-//                case 2: productController.updateProduct(); break;
-//                case 3: productController.deleteProduct(); break;
-//                case 4: productController.selectAllProducts(); break;
-//                case 5: productController.selectProductByCode(); break;
+                case 2: productController.updateProduct(inputModifyMenu()); break;
+                case 3: productController.deleteProduct(inputMenuCode()); break;
+                case 4: productController.selectAllProducts(); break;
+//               case 5: productController.selectProductByCode(); break;
                 case 0: return;
             }
         } while (true);
@@ -43,6 +43,42 @@ public class ProductView {
         System.out.print("추가할 상품 세부사항을 입력해주세요 : ");
         String productDetail = sc.nextLine();
 
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("productCode",productCode);
+        parameter.put("productName",productName);
+        parameter.put("categoryCode",categoryCode);
+        parameter.put("productQuantity",productQuantity);
+        parameter.put("productDetail", productDetail);
+        return parameter;
+
+    }
+
+    private  static Map<String, String > inputMenuCode() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("상품 코드를 입력하세요 :");
+        String code = sc.nextLine();
+
+        Map<String, String> parameter = new HashMap<>();
+        parameter.put("code",code);
+
+        return parameter;
+
+    }
+
+    private static Map<String, String> inputModifyMenu(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("수정할 상품 코드를 입력하세요 :");
+        String productCode = sc.nextLine();
+        System.out.print("수정할 상품 이름을 입력하세요 : ");
+        String productName = sc.nextLine();
+        System.out.print("수정할 카테고리 코드를 입력하세요 : ");
+        String categoryCode = sc.nextLine();
+        System.out.print("수정할 상품 수량을 입력하세요 : ");
+        String productQuantity = sc.nextLine();
+        System.out.print("수정할 상품 세부사항을 작성하세요 : ");
+        String productDetail = sc.nextLine();
 
         Map<String, String> parameter = new HashMap<>();
         parameter.put("productCode",productCode);
